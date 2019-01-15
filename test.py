36b756216de92fb14b3b6a23b8e943e8666520dd
2424655245@qq.com <15246843315@163.com>
2019-01-15 17:18:27 +0800

    init files

---
+++ test.py
@@ -0,0 +1,5 @@
+search='168'
+num_a='1386-168-0006'
+num_b='1681-222-0006'
+print(search+' is at '+str(num_a.find(search)+1)+' to '+str(num_a.find(search)+len(search))+' of num_a ')
+print(search+' is at '+str(num_b.find(search)+1)+' to '+str(num_b.find(search)+len(search))+' of num_b ')
